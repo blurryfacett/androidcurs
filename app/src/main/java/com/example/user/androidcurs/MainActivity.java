fdd5675f72be4210f01bf38d4d3653069a885a79
blurryfacett <blurryface.836@gmail.com>
2021-01-23 20:41:19 +0700

    Finale

---
+++ app/src/main/java/com/example/user/androidcurs/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.user.androidcurs;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GLSurfaceView mGLSurfaceView = new GLSurfaceView(this);
+        mGLSurfaceView.setEGLContextClientVersion(2);
+        ShadowsRenderer renderer = new ShadowsRenderer(this, this);
+        mGLSurfaceView.setRenderer(renderer);
+        setContentView(mGLSurfaceView);
+    }
+}
